9d564756a404dde1683ae807f3162f4c927ace05
comming-soon <himal9626@gmail.com>
2021-09-09 20:37:08 +0530

    Add files via upload

---
+++ urlapp/urls.py
@@ -0,0 +1,10 @@
+from django import urls
+from django.contrib import admin
+from django.urls import path,include
+
+
+urlpatterns = [
+    path('admin',admin.site.urls),
+    path('',include('rootapp.urls'))
+
+]
